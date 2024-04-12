2c54e906637bd1617503c648dfa6877226db1e33
ajs394 <ajs394@gmail.com>
2024-04-12 15:30:30 -0400

    Apparently I moved credentials

--- calendar_events.py
+++ calendar_events.py
@@ -39,7 +39,7 @@ def get_service():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'nyurban-schedule-scraper/credentials.json', SCOPES)
+                    'credentials.json', SCOPES)
                 creds = flow.run_local_server()
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
